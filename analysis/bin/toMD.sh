2f6428f00a1b3274080d7f818a1fe58581b2fa50
David Streever <dstreev@e01.streever.local>
2020-11-03 12:37:01 -0500

    fixed pipes on rhel/centos

--- analysis/bin/toMD.sh
+++ analysis/bin/toMD.sh
@@ -11,7 +11,7 @@ head -n 1 $FILE.md > $FILE.hdr
 
 # Get the first line and build a header row from it.
 # Strip letters, Strip '_', Strip '.', Add header, remove last overrun header, fix (centos sed) double pipes.
-cat $FILE.hdr | sed 's/\([a-z]\)//g' | sed 's/_//g' | sed 's/\.//g' | sed 's/\|/\|:---/g' | sed 's/:---$//g' | sed 's/\|\|/\|/g' >> $FILE.hdr
+cat $FILE.hdr | sed 's/\([a-z]\)//g' | sed 's/_//g' | sed 's/\.//g' | sed 's/\|/\|:---/g' | sed 's/:---$//g' | sed 's/||/|/g' >> $FILE.hdr
 #cat $FILE.hdr | sed 's/\([a-z]\)//g' | sed 's/_//g' | sed 's/\.//g' | sed 's/\|/\| /g' | sed 's/ $//g' | sed 's/ /:---/g' >> $FILE.hdr
 #cat $FILE.hdr | sed 's/\([a-z]\)//g' | sed 's/_//g' | sed 's/ $//g' | sed 's/\.//g' | sed 's/ /:---/g' >> $FILE.hdr
 
